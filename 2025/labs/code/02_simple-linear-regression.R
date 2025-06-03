8b3e5d75b73a2e218531e4c0870a2d00c56fe91a
Jonathan Pipping <jpipping@wharton.upenn.edu>
2025-06-03 15:09:52 -0400

    lab 2 starter code

---
+++ 2025/labs/code/02_simple-linear-regression.R
@@ -0,0 +1,21 @@
+#############
+### SETUP ###
+#############
+
+# install.packages(c("tidyverse")
+library(tidyverse)
+
+##############
+### PART 1 ###
+##############
+
+# load data
+mlb_team_seasons = read_csv("filepath.csv")
+
+
+##############
+### PART 2 ###
+##############
+
+# load data
+mlb_payrolls = read_csv("filepath.csv")
