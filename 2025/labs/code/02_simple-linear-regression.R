b8bc6d92226f843c3c60dfc02e63a87398e2c184
Jonathan Pipping <jpipping@wharton.upenn.edu>
2025-06-03 15:17:52 -0400

    revised starter code

--- 2025/labs/code/02_simple-linear-regression.R
+++ 2025/labs/code/02_simple-linear-regression.R
@@ -5,17 +5,19 @@
 # install.packages("tidyverse")
 library(tidyverse)
 
+# set working directory to wherever your data is
+setwd("filepath")
+
 ##############
 ### PART 1 ###
 ##############
 
 # load data
-mlb_team_seasons = read_csv("filepath.csv")
-
+mlb_team_seasons = read_csv("mlb_team_seasons.csv")
 
 ##############
 ### PART 2 ###
 ##############
 
 # load data
-mlb_payrolls = read_csv("filepath.csv")
+mlb_payrolls = read_csv("mlb_payrolls.csv")
