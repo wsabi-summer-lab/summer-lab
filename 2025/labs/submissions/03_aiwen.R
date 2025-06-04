0a6fe9b6ec69235f60c988e8466893af87c77e92
aiwenli123 <ali52@hawk.iit.edu>
2025-06-04 16:57:49 -0400

    03 submission

--- 2025/labs/submissions/03_aiwen.R
+++ 2025/labs/submissions/03_aiwen.R
@@ -73,15 +73,28 @@ ordered_coefs <- sort(abs(coefs), decreasing = TRUE)
 ordered_coefs
 
 # comparing two models
+set.seed(3)
 n <- dim(nba_four_factors)[1]
-train_indices <- sample(1:n, size = floor(0.5 * n))
+train_indices <- sample(1:n, size = floor(0.8 * n))
 
 train <- nba_four_factors[train_indices, ]
 test <- nba_four_factors[-train_indices, ]
-names(train)
 
 mod1 <- lm(W ~ x1 + x2 + x3 + x4, data = train)
-mod2 <- lm(W ~ x1 + x2 + x3 + x4, data = train_std)
+mod2 <- lm(W ~ x1_std + x2_std + x3_std + x4_std, data = train)
+
+pred1 <- predict(mod1, newdata = test)
+pred2 <- predict(mod2, newdata = test)
+
+# RMSE
+rmse1 <- sqrt(mean((test$W - pred1)^2))
+rmse2 <- sqrt(mean((test$W - pred2)^2))
+rmse1 == rmse2 # false
+rmse1 > rmse2 # false
+rmse1 < rmse2 # true
+
+print(c("Model 1 RMSE" = rmse1, "Model 2 RMSE (standardized x)" = rmse2))
+# rmse1 (without std) has slightly lower RMSE than rmse2 (with std), but they're pretty much the same
 
 ##############
 ### PART 2 ###
@@ -89,7 +102,64 @@ mod2 <- lm(W ~ x1 + x2 + x3 + x4, data = train_std)
 
 # load data
 punts <- read_csv("../data/03_punts.csv")
-names(punts)
+names(punts) # y = next_ydl
 
 # scatterplots
+ggplot(punts, aes(x = ydl, y = next_ydl)) +
+  geom_point() # quadratic? or splines?
 
+ggplot(punts, aes(x = pq, y = next_ydl)) +
+  geom_point() # splines?
+
+# quad: model with quadratic terms for both x variables
+q_model = lm(next_ydl ~ ydl + I(ydl**2) + pq + I(pq**2), data = punts)
+summary(q_model)
+
+spline_model = lm(next_ydl ~ splines::bs(ydl, degree = 3, df = 5) + splines::bs(pq, degree = 3, df = 5),
+                  data = punts)
+summary(spline_model)
+
+# selecting model: spline vs. quadratic
+n <- dim(punts)[1]
+train_indices <- sample(1:n, size = floor(0.8 * n))
+
+train <- punts[train_indices, ]
+test <- punts[-train_indices, ]
+
+mod1 <- lm(next_ydl ~ ydl + I(ydl**2) + pq + I(pq**2), data = train)
+mod2 <- lm(next_ydl ~ splines::bs(ydl, degree = 3, df = 5) + splines::bs(pq, degree = 3, df = 5),
+           data = train)
+
+pred1 <- predict(mod1, newdata = test)
+pred2 <- predict(mod2, newdata = test)
+
+# RMSE
+rmse1 <- sqrt(mean((test$next_ydl - pred1)^2))
+rmse2 <- sqrt(mean((test$next_ydl - pred2)^2))
+
+print(c("Model 1 RMSE" = rmse1, "Model 2 RMSE (standardized x)" = rmse2))
+rmse2 < rmse1 # true, therefore splines model is better
+
+# new col for expected next_ydl based on splines model
+names(punts)
+punts <- punts %>% 
+  mutate(exp_next_ydl = predict(spline_model, newdata = punts))
+
+# expected next yard line vs current yard line for various punter quality values
+
+ggplot(punts, aes(x = ydl, y = exp_next_ydl, color = pq)) +
+  geom_point(alpha = 0.5) +
+  labs(title = "Spline: Expected vs Current Yard Line",
+       x = "Current Yard Line (ydl)",
+       y = "Expected Next Yard Line") +
+  theme_minimal()
+
+punters <- punts %>% 
+  mutate(pyoe = next_ydl - exp_next_ydl) %>% 
+  group_by(punter) %>% 
+  summarise(pyoe = mean(pyoe, na.rm = TRUE)) %>%
+  arrange(desc(pyoe))%>%
+  mutate(punter = factor(punter, levels = unique(punter)))
+
+ggplot(punters, aes(x = punter, y = pyoe))+
+  geom_col(fill = "steelblue")
