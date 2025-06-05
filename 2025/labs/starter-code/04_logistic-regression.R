d854656710e40924fe9df890b531559f10f09d20
Jonathan Pipping <jpipping@wharton.upenn.edu>
2025-06-05 12:14:53 -0400

    lab 4 upload

---
+++ 2025/labs/starter-code/04_logistic-regression.R
@@ -0,0 +1,22 @@
+#############
+### SETUP ###
+#############
+
+# install.packages(c("ggplot2", "tidyverse"))
+library(ggplot2)
+library(tidyverse)
+
+##############
+### PART 1 ###
+##############
+
+# load data
+field_goals = read_csv("../data/04_field-goals.csv")
+
+##############
+### PART 2 ###
+##############
+
+# load data
+ncaab_results = read_csv("../data/04_ncaab-results.csv")
+ncaab_team_info = read_csv("../data/04_ncaab-teams.csv")
