40fb69202de032e131c6b5349c006819fd80682c
Jonathan Pipping <jpipping@wharton.upenn.edu>
2025-06-08 19:32:39 -0400

    lab 6 upload

---
+++ 2025/labs/starter-code/06_models-do-what-they're-told.R
@@ -0,0 +1,16 @@
+#############
+### SETUP ###
+#############
+
+# install.packages(c("ggplot2", "nnet", "splines", "tidyverse"))
+library(ggplot2)
+library(nnet)
+library(splines)
+library(tidyverse)
+
+#######################
+### EXPECTED POINTS ###
+#######################
+
+# load data
+nfl_data = read_csv("../data/06_expected-points.csv")
