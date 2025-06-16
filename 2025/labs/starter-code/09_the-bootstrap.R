9c06be0213b39c2a481d7189d16bf4abdf094242
Noahsonnenklar <noahson@sas.upenn.edu>
2025-06-16 18:57:45 -0400

    j

--- 2025/labs/starter-code/09_the-bootstrap.R
+++ 2025/labs/starter-code/09_the-bootstrap.R
@@ -9,22 +9,6 @@ library(tidyverse)
 # set seed
 set.seed(9)
 
-################
-### SKITTLES ###
-################
-
-# number of each color
-n_red = 10
-n_orange = 10
-n_yellow = 10
-n_green = 10
-n_purple = 10
-# total number of skittles
-n_total = n_red + n_orange + n_yellow + n_green + n_purple
-
-# construct a vector of colors
-skittles = c(rep("red", n_red), rep("orange", n_orange), rep("yellow", n_yellow), rep("green", n_green), rep("purple", n_purple))
-
 #######################
 ### NBA FREE THROWS ###
 #######################
