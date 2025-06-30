53cdf80fa0168700cdc93b78a345c54c1f477d8f
Jonathan Pipping <jpipping@wharton.upenn.edu>
2025-06-30 06:02:08 -0400

    starter code 20

---
+++ 2025/labs/starter-code/20_game-theory.R
@@ -0,0 +1,20 @@
+#############
+### SETUP ###
+#############
+
+# install.packages(c("plotly", "tidyverse"))
+library(plotly)
+library(tidyverse)
+
+# set seed
+set.seed(20)
+
+###########################
+### EQUAL STRENGTH PK'S ###
+##########################
+
+
+
+#######################
+### IMBALANCED PK'S ###
+#######################
